17564e7ddc702d30851eba48946efa0396c97407
Pieter Venter <dodgyrabbit@hotmail.com>
2018-09-13 11:37:47 -0400

    WIP

--- src/dodgyrabbit.MidiBle/IDBusProperties.cs
+++ src/dodgyrabbit.MidiBle/IDBusProperties.cs
@@ -4,7 +4,11 @@ using Tmds.DBus;
 
 namespace dodgyrabbit.MidiBle
 {
-
+    /// <summary>
+    /// This class is probably redundant and here because of a lack of understanding.
+    /// It seems like any time you implement a DBusINterface you are expected to have
+    /// some of these methods (Like GetAll).
+    /// </summary>
     [DBusInterface("org.freedesktop.DBus.Properties")]
     public interface IDBusProperties
     {
--- src/dodgyrabbit.MidiBle/Service.cs
+++ src/dodgyrabbit.MidiBle/Service.cs
@@ -1,8 +1,13 @@
 using Tmds.DBus;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace dodgyrabbit.MidiBle
 {
-
+    [DBusInterface("org.bluez.GattService1")]
     public class Service : IDBusObject
     {
         ObjectPath objectPath;
@@ -14,5 +19,32 @@ namespace dodgyrabbit.MidiBle
         public ObjectPath ObjectPath => objectPath;
         // TODO: Return the required dictionary object here.
         // TODO: Create interface for GattService1. Implement it here.
+
+        /// <summary>
+        /// Inspects the current type and returns the DBusInterface.
+        /// </summary>
+        /// <returns>The DBusInterface Name as decorated by the DBusInterfaceAttribute.</returns>
+        protected string GetInterfaceName()
+        {
+            Attribute attribute = this.GetType().GetCustomAttribute(typeof(DBusInterfaceAttribute));
+            DBusInterfaceAttribute dBusInterface = attribute as DBusInterfaceAttribute;
+            if (dBusInterface == null)
+            {
+                throw new InvalidOperationException("This type does not have the DBusInterfaceAttribute");
+            }
+            return dBusInterface.Name;
+        }
+
+        public Task<ServiceProperties> GetAllAsync()
+        {
+            return null;
+        }
     }
+
+    [Dictionary]
+    public class ServiceProperties
+    {
+
+    }
+
 }
