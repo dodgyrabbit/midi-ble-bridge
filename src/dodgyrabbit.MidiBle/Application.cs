22c410006011cd865a636fe50cf8f7d13ebf814f
Pieter Venter <dodgyrabbit@hotmail.com>
2018-09-13 07:27:58 -0400

    Clean up method

--- src/dodgyrabbit.MidiBle/Application.cs
+++ src/dodgyrabbit.MidiBle/Application.cs
@@ -8,20 +8,11 @@ using Tmds.DBus;
 [assembly: InternalsVisibleTo(Tmds.DBus.Connection.DynamicAssemblyName)]
 namespace dodgyrabbit.MidiBle
 {
-    // Do I need to override this? Would that break something?
-    // Should be at "org.freedesktop.DBus.ObjectManager"
-    // which is what the base class provide.
-    //
-    // This interface does not seem to exist, but it's used on the Python document
-    //[DBusInterface("org.bluez.GattApplication1")]
-
     // TODO:
     //  Currently getting a serialization error.
     //  Do we seperate out the interface from the class? Maybe that is a critical part of how the DBus plumbing works
 
     // Tried a bunch of things, getting error now:
-    // Tmds.DBus.DBusException: org.bluez.Error.Failed: No object received
-    // This looks like it's happening from bluetoothd (prior to DBus?) as it's not yet on DBus
     // sudo btmon
     // Enable verbose logging probably required:
     // https://stackoverflow.com/questions/37003147/i-want-to-enable-debug-messages-on-bluez
@@ -35,7 +26,6 @@ namespace dodgyrabbit.MidiBle
         }
     }
 
-    //[DBusInterface("org.freedesktop.DBus.ObjectManager")]
     public class Application : IObjectManager
     {
         ObjectPath objectPath;
@@ -48,29 +38,29 @@ namespace dodgyrabbit.MidiBle
 
         public async Task<IDictionary<ObjectPath, IDictionary<string, IDictionary<string, object>>>> GetManagedObjectsAsync()
         {
-            var result = new Dictionary<ObjectPath, IDictionary<string, IDictionary<string, object>>>();
+            var objects = new Dictionary<ObjectPath, IDictionary<string, IDictionary<string, object>>>();
             IDictionary<string, IDictionary<string, object>> service = new Dictionary<string, IDictionary<string, object>>();
-            IDictionary<string, IDictionary<string, object>> service2 = new Dictionary<string, IDictionary<string, object>>();
 
+            IDictionary<string, IDictionary<string, object>> serviceCharacteristic = new Dictionary<string, IDictionary<string, object>>();
             IDictionary<string, object> serviceCharacteristics = new Dictionary<string, object>();
+            serviceCharacteristics["Descriptors"] = new ObjectPath[] {};
+            serviceCharacteristics["Flags"] = new string[] {"notify"};
+            // Heart rate measurement
             serviceCharacteristics["UUID"] = "00002a37-0000-1000-8000-00805f9b34fb";
             serviceCharacteristics["Service"] = new ObjectPath("/org/bluez/example/service0");
-            serviceCharacteristics["Flags"] = new string[] {"notify"};
-            serviceCharacteristics["Descriptors"] = new Object[] {};
-            service["org.bluez.GattCharacteristic1"] = serviceCharacteristics;
+            serviceCharacteristic["org.bluez.GattCharacteristic1"] = serviceCharacteristics;
 
-            IDictionary<string, object> service0 = new Dictionary<string, object>();
-            service0["Characteristics"] = new ObjectPath[] {new ObjectPath("/org/bluez/example/service0/char0")};
-            service0["UUID"] = "0000180d-0000-1000-8000-00805f9b34fb";
-            service0["Primary"] = true;
-            service2["org.bluez.GattService1"] = service0;
+            IDictionary<string, object> serviceDetails = new Dictionary<string, object>();
+            serviceDetails["Characteristics"] = new ObjectPath[] {new ObjectPath("/org/bluez/example/service0/char0")};
+            // Heart rate service
+            serviceDetails["UUID"] = "0000180d-0000-1000-8000-00805f9b34fb";
+            serviceDetails["Primary"] = true;
+            service["org.bluez.GattService1"] = serviceDetails;
 
-            result[new ObjectPath("/org/bluez/example/service0/char0")] = service;
-            result[new ObjectPath("/org/bluez/example/service0")] = service2;
-            return result;
+            objects[new ObjectPath("/org/bluez/example/service0/char0")] = serviceCharacteristic;
+            objects[new ObjectPath("/org/bluez/example/service0")] = service;
+            return objects;
         }
-
-        
 
         public async Task<IDisposable>  WatchInterfacesAddedAsync(Action<(ObjectPath @object, IDictionary<string, IDictionary<string, object>> interfaces)> handler, Action<Exception> onError)
         {
